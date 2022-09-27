85bc4b89012b822de642104d767eb4a76d75da24
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-09-27 16:46:14 +0100

    Test Driving a Repository Class Challenge Part 1

---
+++ lib/database_connection.rb
@@ -0,0 +1,22 @@
+# file: lib/database_connection.rb
+
+require 'pg'
+
+# This class is a thin "wrapper" around the
+# PG library. We'll use it in our project to interact
+# with the database using SQL.
+
+class DatabaseConnection
+  def self.connect(book_store)
+    @connection = PG.connect({ host: '127.0.0.1', dbname: book_store })
+  end
+
+  def self.exec_params(query, params)
+    if @connection.nil?
+      raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
+      'the database was never opened. Did you make sure to call first the method '\
+      '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
+    end
+    @connection.exec_params(query, params)
+  end
+end
